9af59ca0898d21824c7604d2e2314219f75b9863
Robert Mullins <mullinsr@live.com>
2014-12-03 01:41:07 -0600

    Create related_products_on.php
    Turns on related products option for all products in a Bigcommerce store.

---
+++ related_products_on.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Script to turn on related products for ALL products
+ * @author Robert Mullins <robert.mullins@bigcommerce.com>
+ * @license The MIT License (MIT)
+ */
+ 
+require 'bigcommerce.php';
+use Bigcommerce\Api\Client as Bigcommerce;
+
+/* API Connection */
+Bigcommerce::configure(array(
+			'store_url' => 'https://store-********.mybigcommerce.com',
+			'username' => '*******', 
+			'api_key' => '***********************'
+		));
+Bigcommerce::verifyPeer(false);
+Bigcommerce::setCipher('RC4-SHA');
+
+$num_of_products = Bigcommerce::getProductsCount();
+
+if ($num_of_products <= 250) {
+    $max_pages = 1;
+} else {
+    $pages_double = ($num_of_products/250);
+    $pages_int = ((int)($num_of_products/250));
+    if ($pages_double > $pages_int) { 
+        $max_pages = $pages_int + 1;
+    } else {
+        $max_pages = $pages_int; //If 250 goes into number of products evenly (rare)
+    }
+}
+
+$fields = array(
+    'related_products' => '-1'
+);
+
+
+$processed = array(); //Collection returns duplicates sometimes, this will hold the ID of all products we processed. 
+
+$current_page = 1;
+while ($current_page <= $max_pages) {
+	$filter = array(
+    'page' => $current_page,
+    'limit' => 250
+);
+
+	$products = Bigcommerce::getProducts($filter);
+	foreach ($products as $product) {
+    		$id = $product->id;
+    		if (!in_array($id, $processed)){
+        		Bigcommerce::updateProduct($id, $fields);
+         	array_push($processed, $id);
+    		}    
+	}
+	++$current_page;
+}
+?>
