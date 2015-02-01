76ea4784ff02e844a6d042050ae426652830e94e
Robert Mullins <mullinsr@live.com>
2015-02-01 00:04:21 -0500

    Create wholesale.js

---
+++ wholesale.js
@@ -0,0 +1,25 @@
+<!-- Script to detect Retail/Wholesale Customer .. Performs some action -->
+<!-- Must create category called WholesaleJS and assign to appropriate customer group -->
+<script>
+$(document).ready(function() {
+    var wholesaler = false;
+    var price = document.querySelector(".Left .SideCategoryListRetail");
+    $('div.SideCategoryListFlyout li').each(function(index) {
+        var str1 = $(this).text();
+        var str2 = "WholesaleJS";
+        if(str1.indexOf(str2) != -1) {
+            wholesaler = true;
+        };
+    });      
+    $("li:contains('WholesaleJS')").css("display","none");
+
+    if(wholesaler == false) {
+        //Hide pricing and add to cart functions here. Varies based on theme. Example:
+        //var price = document.querySelector("ENTER ITS DIV CLASS HERE");
+        //var cart = document.querySelector("ENTER ITS DIV CLASS HERE");
+        //price.style.display = "none";
+        //cart.style.display = "none";
+
+    };
+});
+</script>
